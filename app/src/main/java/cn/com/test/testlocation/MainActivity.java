ab1f10fb5b036c2582c6b9fdf74abffc32c2ee3c
luoaojin <luoaojin@bonc.com.cn>
2020-07-06 19:56:39 +0800

    fix()

--- app/src/main/java/cn/com/test/testlocation/MainActivity.java
+++ app/src/main/java/cn/com/test/testlocation/MainActivity.java
@@ -111,7 +111,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 //        openGPS(this);
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
             //    ActivityCompat#requestPermissions
             // here to request the missing permissions, and then overriding
